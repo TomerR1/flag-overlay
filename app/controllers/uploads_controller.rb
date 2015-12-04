eee816dc4ff897560fca907497f522e0bed7fe2b
Tomer Ron <tomeron92@gmail.com>
2015-12-04 23:07:49 +0000

    Added uploads controller, new create show, upload form, assigning random keys to images and displaying them by key

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,2 +1,21 @@
 class UploadsController < ApplicationController
+    def new
+        @upload = Upload.new
+    end
+    def create
+        @upload = Upload.new(upload_params)
+        @upload.key = SecureRandom.hex(5).to_s
+        if @upload.save
+            redirect_to upload_path(@upload)
+        else
+            redirect_to root_path
+        end
+    end
+    def show
+        @upload = Upload.find_by key: params[:key]
+    end
+    private
+        def upload_params
+            params.require(:upload).permit(:image)
+        end
 end
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,3 +1,8 @@
 class Upload < ActiveRecord::Base
     has_attached_file :image
+    validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
+    validates :key, presence: true
+    def to_param
+        key
+    end
 end
