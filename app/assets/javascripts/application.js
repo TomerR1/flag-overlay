efbba932e392efb1705016c95075d144c334750e
Tomer Ron <tomeron92@gmail.com>
2015-12-18 03:28:26 +0000

    Implented HTML5 Canvas to process the images in client side

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,12 @@
 
 $(document).ready(function () {
     $("#flag").css({
-      "width":$("#img img").width() - 2
+      "width":$("#img img").width(),
+      "height":$("#img img").height()
+    });
+    $("#overlay").css({
+      "width":$("#img img").width(),
+      "height":$("#img img").height()
     });
     $("#mix").change(function() {
         $("#img img").css("opacity", this.value);
@@ -27,18 +32,37 @@ $(document).ready(function () {
     });
     $("#fit").click(function() {
         $("#flag").css({
-            "width":$("#img img").width()
+            "left":0,
+            "top":0,
+            "width":$("#img img").width(),
+            "height":$("#img img").height()
         });
     });
     $("#go").click(function() {
-        html2canvas($("#flag"), {
+        html2canvas($("#overlay"), {
             allowTaint: true,
             taintTest: true,
             onrendered: function(canvas) {
-               $("#result").append(canvas);
+                var canvas2 = document.getElementById("canvas2");
+                $("#canvas2").css({
+                    "width":$("#flag").width(),
+                    "height":$("#flag").height()
+                });
+                var context = canvas2.getContext('2d');
+                
+                var imageObj = new Image();
+                imageObj.onload = function() {
+                    context.clearRect(0, 0, canvas2.width, canvas2.height);
+                    canvas2.width = $("#flag").width();
+                    canvas2.height = $("#flag").height();
+                    var left = $('#flag').css( "left" ).replace(/[^-\d\.]/g, '');;
+                    var top = $('#flag').css( "top" ).replace(/[^-\d\.]/g, '');;
+                    context.drawImage(imageObj, left, top, $("#flag").width(), $("#flag").height(), 0, 0, $("#flag").width(), $("#flag").height());
+                };
+                imageObj.src = canvas.toDataURL();
             }
         });
     });
     $("#flag").draggable({ containment: $("#img img") });
-    $("#flag").resizable({ containment: $("#img img"), handles: 'ne, se, sw, nw', aspectRatio: 'true' });
+    $("#flag").resizable({ containment: $("#img img"), handles: 'ne, se, sw, nw' });
 });
