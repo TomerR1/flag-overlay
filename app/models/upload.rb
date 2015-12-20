3d7717ceff8ab96f7f0b359d3b29ea2c9c4b9a1e
Tomer Ron <tomeron92@gmail.com>
2015-12-20 00:34:29 +0000

    Added new thumbnail paperclip setting for image uploads, and added new resized setting for pictures larger than 800x800

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,5 +1,8 @@
 class Upload < ActiveRecord::Base
-    has_attached_file :image
+    has_attached_file :image,
+        :styles => {
+      :thumb => "100x100",
+      :resized  => "800x800>" }
     validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
     validates :key, presence: true
     def to_param
